8debf1f4dd973b4c113d9241f78e907e6b3c2dcb
Rafael Tolentino <rafael09tolentino@gmail.com>
2025-08-13 23:28:47 +0800

    defined app routes

--- frontend/finance-tracker/src/App.jsx
+++ frontend/finance-tracker/src/App.jsx
@@ -1,9 +1,38 @@
 import React from "react"  
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Login from "./pages/Auth/Login";
+import Signup from "./pages/Auth/Signup";
+import Home from "./pages/Dashboard/Home";
+import Income from "./pages/Dashboard/Income";
+import Expenses from "./pages/Dashboard/Expenses";
+
 const App = () => {
   return (
-    <div>Finance Tracker App</div>
-  )
-}
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/signup" exact element={<Signup />} />
+          <Route path="/dashboard" exact element={<Home />} />
+          <Route path="/dashboard/income" exact element={<Income />} />
+          <Route path="/dashboard/expenses" exact element={<Expenses />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
 
-export default App
+export default App;
+
+const Root = () => {
+  const isAuthenticated = !!localStorage.getItem("token");  
+
+  return isAuthenticated ? <Navigate to="/dashboard" /> : <Navigate to="/login" />;
+};
