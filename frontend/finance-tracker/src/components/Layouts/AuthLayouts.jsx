bb06e08871631a7de8acc8f15f8600bea921e5c9
Rafael Tolentino <rafael09tolentino@gmail.com>
2025-08-13 23:46:11 +0800

    started Login Layout component

---
+++ frontend/finance-tracker/src/components/Layouts/AuthLayouts.jsx
@@ -0,0 +1,13 @@
+import React from "react"  
+
+const AuthLayouts = ({ children }) => {
+  return <div>
+    <div className="w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12">
+        <h2 className="text-2xl mb-4">Finance Tracker</h2>
+        {children}
+        </div>
+    </div>
+}
+
+export default AuthLayouts
+
--- frontend/finance-tracker/src/pages/Auth/Login.jsx
+++ frontend/finance-tracker/src/pages/Auth/Login.jsx
@@ -1,8 +1,14 @@
-import React from "react"  
+import React from "react"
+import AuthLayouts from "../../components/Layouts/AuthLayouts";  
 
 const Login = () => {
   return (
-    <div>Login Page</div>
+    <AuthLayouts>
+        <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
+            <h3 className="text-xl font-semibold">Welcome Back!</h3>
+            <p className="text-xs">Please enter your credentials to Log In.</p>
+        </div>
+    </AuthLayouts>
   )
 }
 
