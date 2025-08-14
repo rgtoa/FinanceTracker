a58262733bf45f1f7aca56d6065272221362d6c9
Rafael Tolentino <rafael09tolentino@gmail.com>
2025-08-14 22:39:44 +0800

    added UI for Login + started modifying Login Handling

--- frontend/finance-tracker/src/components/Layouts/AuthLayouts.jsx
+++ frontend/finance-tracker/src/components/Layouts/AuthLayouts.jsx
@@ -1,13 +1,48 @@
-import React from "react"  
+import React from "react"
+import { LuTrendingUpDown } from "react-icons/lu";
+import CARD_2 from "../../assets/images/card_2.png"
 
 const AuthLayouts = ({ children }) => {
-  return <div>
+  return <div className="flex">
     <div className="w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12">
-        <h2 className="text-2xl mb-4">Finance Tracker</h2>
+        <h2 className="text-lg font-medium text-black">Finance Tracker</h2>
         {children}
-        </div>
     </div>
+
+    <div className="hidden md:block w-[40vw] h-screen bg-[#F8F8F9] bg-auth-bg-img bg-cover bg-no-repeat bg-center overflow-hidden p-8 relative">
+        <div className="w-48 h-48 rounded-[40px] bg-purple-600 absolute -top-7 -left-5" />
+        <div className="w-48 h-48 rounded-[40px] border-[20px] border-fuchsia-600 absolute top-[30%] right-10" />
+        <div className="w-48 h-48 rounded-[40px] bg-violet-500 absolute -bottom-7 -left-5" />
+
+        <div className="grid grid-cols-1 z-20">
+          <StatsInfoCard
+            icon={<LuTrendingUpDown />}
+            label="Track your total Income and Expenses"
+            value="69,000"
+            color="bg-primary"
+          />
+        </div>
+
+        <img 
+          src={CARD_2} 
+          className="w-64 lg:w-[90%] absolute bottom-10 shadow-blue-400/15 right-10" 
+        />
+    </div>
+  </div>;
 }
 
 export default AuthLayouts
 
+const StatsInfoCard = ({ icon, label, value, color }) =>{
+  return <div className="flex gap-6 bg-white p-4 rounded-xl shadow-md shadow-purple-400/10 border border-gray-200/50 z-10">
+    <div 
+      className={`w-12 h-12 flex items-center justify-center text-[25px] text-white ${color} rounded-full drop-shadow-xl`}
+    >
+      {icon}
+    </div>
+    <div>
+      <h6 className="text-xs text-gray-500 mb-1">{label}</h6>
+      <span className="text-[20px]">${value}</span>
+    </div>
+  </div>
+}
--- frontend/finance-tracker/src/pages/Auth/Login.jsx
+++ frontend/finance-tracker/src/pages/Auth/Login.jsx
@@ -2,11 +2,32 @@ import React from "react"
 import AuthLayouts from "../../components/Layouts/AuthLayouts";  
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+
+  }
+
   return (
     <AuthLayouts>
         <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
             <h3 className="text-xl font-semibold">Welcome Back!</h3>
-            <p className="text-xs">Please enter your credentials to Log In.</p>
+            <p className="text-xs">
+              Please enter your credentials to Log In.
+            </p>
+
+            <form onSubmit={handleLogin}>
+              <Input
+                value={email}
+                onChange={({target}) => setEmail(target.value)}
+                label="Email Address"
+                placeholder="youremail@example.com"
+                type="text"
+              />
+            </form>
         </div>
     </AuthLayouts>
   )
